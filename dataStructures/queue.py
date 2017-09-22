c4f069334bca26e9f1b1bd1909cd0023e99dbc4b
tannmay <tanmayy98@gmail.com>
2017-09-22 08:14:33 +0530

    Array implementation of queue

---
+++ dataStructures/queue.py
@@ -0,0 +1,18 @@
+class Queue:
+
+	def __init__(self):
+		self.item = []
+
+	def isEmpty(self):
+		return self.items == []
+
+	def enqueue(self, item):
+		self.items.insert(0,item)
+
+	def dequeue(self):
+		return self.item.pop()
+
+	def size(self):
+		return len(self.items)
+
+		
