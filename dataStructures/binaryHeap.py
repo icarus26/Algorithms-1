7c18de25e4170df58097fead8740e9f65487a452
MPRUDRA <32811681+MPRUDRA@users.noreply.github.com>
2017-10-31 22:03:33 +0530

    Update binaryHeap.py

--- dataStructures/binaryHeap.py
+++ dataStructures/binaryHeap.py
@@ -44,3 +44,11 @@ class MinHeap:
     # Get the minimum element from the heap
     def getMin(self):
         return self.heap[0]
+    
+    #Here is a sample line of code that can be executed in Python:
+
+#print("Hello, World!")
+#You can just as easily store a string as a variable and then print it to stdout:
+
+#my_string = "Hello, World!"
+#print(my_string)
